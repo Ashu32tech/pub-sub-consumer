2acc5ed19cf3ba66b61a91f822ee827900c43319
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2021-03-11 19:07:38 +0530

    pub sub consumer code refactor

--- src/main/java/com/infogain/gcp/poc/pubsub/consumer/PubSubConsumer.java
+++ src/main/java/com/infogain/gcp/poc/pubsub/consumer/PubSubConsumer.java
@@ -1,26 +1,46 @@
 package com.infogain.gcp.poc.pubsub.consumer;
 
-import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.pubsub.v1.PubsubMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
 import org.springframework.cloud.gcp.pubsub.support.BasicAcknowledgeablePubsubMessage;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
 
-public abstract class PubSubConsumer {
+@Slf4j
+@Component
+public class PubSubConsumer {
 
     @Autowired
     private PubSubTemplate pubSubTemplate;
 
-    public abstract  String subscription();
+    @Value("${pub.sub.subscription}")
+    private String subscription;
 
-    protected abstract  void consume(BasicAcknowledgeablePubsubMessage message);
+    private Consumer<BasicAcknowledgeablePubsubMessage> consumer = this::consume;
 
-    public Consumer<BasicAcknowledgeablePubsubMessage> consumer(){
-        return basicAcknowledgeablePubsubMessage -> consume(basicAcknowledgeablePubsubMessage);
+    @EventListener(ApplicationReadyEvent.class)
+    public void subscribe() {
+        log.info("subscribing {} to {}", this.getClass().getSimpleName(), this.subscription);
+        pubSubTemplate.subscribe(subscription, consumer);
     }
 
-    public Subscriber consumeMessage(){
-        return pubSubTemplate.subscribe(this.subscription(), this.consumer());
+    public void consume(BasicAcknowledgeablePubsubMessage basicAcknowledgeablePubsubMessage) {
+        PubsubMessage message = basicAcknowledgeablePubsubMessage.getPubsubMessage();
+
+        try {
+            log.info("message={},", message.getData().toStringUtf8());
+            log.info("message-attribute-map={}", message.getAttributesMap());
+        } catch (Exception ex) {
+            log.error("Error while receiving pub sub message:::::", ex);
+        }
+
+        basicAcknowledgeablePubsubMessage.ack();
     }
+
 }
